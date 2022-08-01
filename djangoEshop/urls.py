cf85fe564313d1d6bb36dc4b41b4eda220b9a8af
tyurinart14 <96427880+tyurinart14@users.noreply.github.com>
2022-08-02 02:07:53 +0300

    Add files via upload
    Made one url_pattern, view and template for all products

---
+++ djangoEshop/urls.py
@@ -0,0 +1,10 @@
+from djangoEshop import view
+from django.urls import path
+
+urlpatterns = [
+    path('', view.homepage),
+    path('homepage/', view.homepage),
+    path('client_page/', view.client_page),
+    path('basket', view.basket),
+    path('product/<str:item_name>/', view.product_all),
+]
