171f9e6f8d294f2f0863e1de57d2c45c4db0ab88
Tiurin Artur <tyurinart14@gmail.com>
2022-08-04 16:36:41 +0300

    Add Category model

--- aplication/admin.py
+++ aplication/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from aplication.models import Product
+from aplication.models import Category
 
 
 admin.site.register(Product)
+admin.site.register(Category)
--- aplication/models.py
+++ aplication/models.py
@@ -7,3 +7,9 @@ class Product(models.Model):
     amount = models.IntegerField(null=True)
     slug = models.SlugField(max_length=32)
     availability = models.BooleanField(null=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=24)
+    slug = models.SlugField(max_length=24)
+    description = models.TextField(null=True, blank=True)
