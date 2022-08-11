3e05cb61e16797f3e0363bfb0ff5fb051ccf6c05
Tiurin Artur <tyurinart14@gmail.com>
2022-08-11 16:58:13 +0300

    Removed unnecessary attributes in the model and corrected the table

--- aplication/admin.py
+++ aplication/admin.py
@@ -3,5 +3,21 @@ from aplication.models import ProductContext
 from aplication.models import Category
 
 
-admin.site.register(ProductContext)
-admin.site.register(Category)
+class ProductContextAdmin(admin.ModelAdmin):
+    list_display = "name", "slug", "cat_id"
+    list_display_links = ["name"]
+    prepopulated_fields = {
+        "slug": ["name"],
+    }
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = "name", "slug", "id"
+    list_display_links = ["name"]
+    prepopulated_fields = {
+        "slug": ["name"],
+    }
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductContext, ProductContextAdmin)
--- aplication/models.py
+++ aplication/models.py
@@ -15,15 +15,6 @@ class ProductContext(models.Model):
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=32)
-    price = models.CharField(max_length=32)
-    amount = models.IntegerField(null=True)
-    slug = models.SlugField(max_length=32)
-    availability = models.BooleanField(null=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
-
-
 class Category(models.Model):
     name = models.CharField(max_length=24)
     slug = models.SlugField(max_length=24)
