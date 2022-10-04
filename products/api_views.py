b210b948af1ca31e7ecf1ce03968b1b1368150e0
Tiurin Artur <tyurinart14@gmail.com>
2022-10-04 17:22:17 +0300

    Add APIViews for Product and Category models

---
+++ products/api_views.py
@@ -0,0 +1,23 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from .serializer import ProductSerializer, CategorySerializer
+from .models import Product, Category
+
+
+class CategoryAPIView(ListCreateAPIView):
+    serializer_class = ProductSerializer
+    model = serializer_class.Meta.model
+
+    def get_queryset(self):
+        cat_id = self.kwargs['num']
+        queryset = self.model.objects.filter(cat_id=cat_id)
+        return queryset
+
+
+class ProductListAPIView(ListCreateAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+class ProductRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,21 @@
-from products import views
+from products import views, api_views
 from django.urls import path
 
 urlpatterns = [
-    path('product/<int:num>/<slug:slug>/', views.product_detail, name="product"),
-    path('homepage/<int:number>/', views.CategoryView.as_view(), name="category"),
-    path('product/add/', views.CreateNewProduct.as_view(), name="add_product"),
-    path('product/change/<slug:slug>/', views.UpdateProductData.as_view(), name="change_product_info")
+    path('product/<int:num>/<slug:slug>/', views.product_detail,
+         name="product"),
+    path('homepage/<int:number>/', views.CategoryView.as_view(),
+         name="category"),
+    path('product/add/', views.CreateNewProduct.as_view(),
+         name="add_product"),
+    path('product/change/<slug:slug>/', views.UpdateProductData.as_view(),
+         name="change_product_info"),
+
+
+    path('api/product-list/', api_views.ProductAPIView.as_view(),
+         name="api-product-list"),
+    path('api/category-list/<int:num>/', api_views.CategoryAPIView.as_view(),
+         name="api-category-view"),
+    path('api/detail-update-product/<int:pk>/', api_views.ProductRetrieveUpdateAPIView.as_view(),
+         name="detail-update-product")
 ]
