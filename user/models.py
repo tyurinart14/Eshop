655c412cf3bf42ec2302b9519bf7a11147eee03c
Tiurin Artur <tyurinart14@gmail.com>
2022-10-28 13:49:45 +0300

    Removed all migrations, tables and user model. Changed the model, returned migrations and tables

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 
 class UserModel(AbstractUser):
-    wallet = models.IntegerField(default=20000)
+    wallet = models.IntegerField(default=100000)
 
     class Meta:
         db_table = "auth_user"
