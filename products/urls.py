468c2e7adbc157cc84a05542779e50b72d56b0ba
Tiurin Artur <tyurinart14@gmail.com>
2022-10-28 18:41:45 +0300

    fix url

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('product/<int:num>/<slug:slug>/', views.product_detail, name="product"),
     path('homepage/<int:number>/', views.CategoryView.as_view(), name="category"),
     path('product/add/', views.CreateNewProduct.as_view(), name="add_product"),
-    path('product/change/<slug:slug>/', views.UpdateProductData.as_view(), name="change_product_info")
+    path('product/change/<pk>/', views.UpdateProductData.as_view(), name="change_product_info")
 ]
