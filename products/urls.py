5946a02bd54a96f377ac5f7ff5d1feb157cb7a78
Tiurin Artur <tyurinart14@gmail.com>
2022-10-28 18:56:42 +0300

    delete slug in product url

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = "products"
 
 urlpatterns = [
-    path('product/<int:num>/<slug:slug>/', views.product_detail, name="product"),
+    path('product/<int:num>/', views.product_detail, name="product"),
     path('homepage/<int:number>/', views.CategoryView.as_view(), name="category"),
     path('product/add/', views.CreateNewProduct.as_view(), name="add_product"),
     path('product/change/<pk>/', views.UpdateProductData.as_view(), name="change_product_info")
--- products/views.py
+++ products/views.py
@@ -17,10 +17,9 @@ class CategoryView(ListView):
         return context
 
 
-def product_detail(request, num, slug):
+def product_detail(request, num):
     product = get_object_or_404(Product,
-                                id=num,
-                                slug=slug)
+                                id=num)
     cart_product_form = CartAddProductForm()
     return render(request, 'product_detail.html', {'product': product,
                                                    'cart_product_form': cart_product_form})
